485233f9749420df1a81e62a25bcc2a549797eab
Alina Trukhina <alina.trukhina@gmail.com>
2022-09-13 23:02:57 -0500

    added addToCollection func

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,19 @@
 console.log('***** Music Collection *****')
+
+// create an empty array to represent collection
+let collection = [];
+
+// create function that makes an object for each album
+
+function addToCollection(title, artist, yearPublished) {
+    const album = {
+        albumTitle : title,
+        albumArtist : artist,
+        yearPublished : yearPublished
+    }
+    collection.push(album);
+    return collection;
+}
+
+console.log('add a record to collection:', addToCollection('1', '2', 1993));
+console.log(collection);
